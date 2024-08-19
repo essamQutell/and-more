a88b2d08be8d98b4117c2ebc791de694813b6711
essamQutell <essamemara@qutell.com>
2024-08-19 18:07:48 +0300

    feat(roles) :  roles and permissions

---
+++ app/Enums/AdminSectionEnum.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Enums\Users;
+
+enum AdminSectionEnum: int
+{
+    case admins = 1;
+    case roles = 2;
+    case users = 3;
+    case settings = 4;
+
+    case projects = 5;
+    case suppliers = 6;
+    case quotations = 7;
+    case services = 8;
+    case statuses = 9;
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::roles => __('application.roles'),
+            self::admins => __('application.admins'),
+            self::users => __('application.users'),
+            self::settings => __('application.settings'),
+            self::projects => __('application.projects'),
+            self::suppliers => __('application.suppliers'),
+            self::quotations => __('application.quotations'),
+            self::services => __('application.services'),
+            self::statuses => __('application.statuses'),
+        };
+    }
+}
