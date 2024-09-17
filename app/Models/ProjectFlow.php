d8f8b09925c9ad93fd6a40ac36bb4aebcbea2ca5
essamQutell <essamemara@qutell.com>
2024-09-17 17:39:30 +0300

    fix(admins) : password

---
+++ app/Models/ProjectFlow.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectFlow extends Model
+{
+    protected $table = 'project_flows';
+
+    protected $guarded = [];
+}
