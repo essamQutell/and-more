d92f22e3a2a31b9a37a085917ac56468f0f8c891
essamQutell <essamemara@qutell.com>
2024-09-18 09:07:16 +0300

    fix(suppliers) : password

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -15,34 +15,29 @@ class SupplierController extends Controller
 {
     use ResponseTrait;
 
-    //todo: supplier index
     public function index(): JsonResponse
     {
         $suppliers = Supplier::paginate(10);
         return self::successResponsePaginate(data: SupplierResource::collection($suppliers)->response()->getData(true));
     }
 
-    //todo: supplier store
     public function store(StoreSupplierRequest $request)
     {
         $supplier = Supplier::create($request->validated());
         return self::successResponse(data: SupplierResource::make($supplier));
     }
 
-//todo: supplier show
     public function show(Supplier $supplier): JsonResponse
     {
         return self::successResponse(data: SupplierResource::make($supplier));
     }
 
-//todo: supplier update
     public function update(UpdateSupplierRequest $request, Supplier $supplier)
     {
         $supplier->update($request->validated());
         return self::successResponse(message: __('admin.updated'), data: SupplierResource::make($supplier));
     }
 
-//todo: supplier destroy
     public function destroy(Supplier $supplier)
     {
         $supplier->delete();
