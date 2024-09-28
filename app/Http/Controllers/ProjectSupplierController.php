fc7fe89746492557ef1d4fd0ed4dc2491080594e
Essam Emara <33706166+essoemmo@users.noreply.github.com>
2024-09-28 22:12:04 +0300

    feat(projects) : add columns to model

--- app/Http/Controllers/ProjectSupplierController.php
+++ app/Http/Controllers/ProjectSupplierController.php
@@ -18,7 +18,7 @@ class ProjectSupplierController extends Controller
         $projectSupplier->quotation->update([
             'actual_cost' =>($projectSupplier->quotation->actual_cost) + ($projectSupplier->actual_cost),
             'cash_flow' => ($projectSupplier->quotation->cash_flow) + ($projectSupplier->deposit),
-            'cash_flow_percentage' => (($projectSupplier->quotation->cash_flow / $projectSupplier->quotation->actual_cost) * 100) ?? 0,
+           // 'cash_flow_percentage' => (($projectSupplier->quotation->cash_flow / $projectSupplier->quotation->actual_cost) * 100) ?? 0,
             'actual_margin'=>($projectSupplier->quotation->total_project_sales) - ($projectSupplier->quotation->actual_cost),
             'variance' => ($projectSupplier->quotation->actual_margin) - ($projectSupplier->quotation->total_margin)
         ]);
