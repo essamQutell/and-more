06ff143dc75dc2241b67d3aef856ba73664d5871
Essam Emara <33706166+essoemmo@users.noreply.github.com>
2024-09-30 20:40:10 +0300

    feat(projects) : add columns to model

--- app/Http/Requests/ServiceRequest.php
+++ app/Http/Requests/ServiceRequest.php
@@ -13,13 +13,14 @@ class ServiceRequest extends ApiFormRequest
             'name_ar' => [Rule::requiredIf($this->routeIs('services.store')), 'string', 'max:255'],
             'name_en' => [Rule::requiredIf($this->routeIs('services.store')), 'string', 'max:255'],
             'parent_id' => ['nullable', 'exists:services,id',function ($attribute, $value, $fail) {
+                if ($this->routeIs('services.update')) {
                 if (!is_null($value)) {
                     $service = Service::find($this->service->id);
                     if ($service && $service->services()->exists()) {
                         $fail ('A main service with sub-services cannot be assigned as a child.');
                     }
                 }
-            }],
+            }}],
         ];
     }
 }
