6aec1974c0d0271fa8611fb5254642a62a399d43
Ahmedelnemr-Qutell <146721089+Ahmedelnemr-Qutell@users.noreply.github.com>
2024-10-02 16:26:48 +0300

    Fix (Service ):  factory

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -17,7 +17,10 @@ class ServiceFactory extends Factory
     public function definition(): array
     {
         return [
-
+            'parent_id' => function () {
+                self::$counter++;
+                return (self::$counter <= 5) ? null : $this->faker->numberBetween(1, 5);
+            },
             'name_ar' => $this->faker->word,
             'name_en' => $this->faker->word
         ];
